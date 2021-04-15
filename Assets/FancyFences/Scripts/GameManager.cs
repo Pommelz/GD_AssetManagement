0aac7b268028cffc3708f857f921f13d256918ee
Tim Jahnke <timjahnke@t-online.de>
2021-04-15 15:53:53 +0200

    PlayScene

--- Assets/FancyFences/Scripts/GameManager.cs
+++ Assets/FancyFences/Scripts/GameManager.cs
@@ -34,6 +34,10 @@ public class GameManager : MonoBehaviour
 
     public bool InEndScreen = false;
 
+    public GameObject WinTrigger;
+
+    public Canvas PauseMenu;
+
 
     void Update()
     {
@@ -46,24 +50,48 @@ public class GameManager : MonoBehaviour
             if (CheckNumber == 2)
             {
                 Checkpoint2.SetActive(false);
+                
             }
             if (CheckNumber == 3)
             {
                 Checkpoint3.SetActive(false);
+                Checkpoint1.SetActive(true);
+            }
+
+            if(CheckNumber == 4)
+            {
+                Checkpoint1.SetActive(false);
+                Checkpoint2.SetActive(true);
+            }
+            if(CheckNumber == 5)
+            {
+                Checkpoint2.SetActive(false);
+                Checkpoint3.SetActive(true);
+            }
+            if(CheckNumber == 6)
+            {
+                Checkpoint1.SetActive(false);
+                Checkpoint2.SetActive(false);
+                Checkpoint3.SetActive(false);
+                WinTrigger.SetActive(true);
             }
         }
 
 
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            PauseMenu.enabled = true;
+        }
 
-        if(CheckNumber == 3 && WIN == true)
+        if( WIN == true)
         {
             //WIN!
             WinScreen.enabled = true;
             PlayeR.SetActive(false);
         }
 
-        CheckpointText.text = "Checkpoint Nummer " + CheckNumber.ToString();
+        CheckpointText.text = "Point " + CheckNumber.ToString();
 
 
         if (takingAway == false && timeLeft > 0 && PlayerWon == false)
@@ -79,9 +107,15 @@ public class GameManager : MonoBehaviour
         takingAway = true;
         yield return new WaitForSeconds(1);
         timeLeft -= 1;
-        textDisplay.GetComponent<Text>().text = " Verbleibende Zeit " + timeLeft;
+        textDisplay.GetComponent<Text>().text = " Time Left " + timeLeft;
         takingAway = false;
 
+    }
+
+    public void ExitPause()
+    {
+        PauseMenu.enabled = false;
+        Time.timeScale = 1f;
     }
 
 }
